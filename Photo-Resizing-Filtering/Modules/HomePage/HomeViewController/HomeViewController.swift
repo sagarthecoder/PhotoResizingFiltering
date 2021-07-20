a601a15841514172366c483520c2917faffa98cf
Sagar <sagar@bitmorpher.com>
2021-07-21 01:05:07 +0600

    Photo resizing done

--- Photo-Resizing-Filtering/Modules/HomePage/HomeViewController/HomeViewController.swift
+++ Photo-Resizing-Filtering/Modules/HomePage/HomeViewController/HomeViewController.swift
@@ -6,16 +6,106 @@
 //
 
 import UIKit
+import Photos
+import CoreServices
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate, SavePhotoToLibraryDelegate {
 
+    @IBOutlet weak var photoResizingButton: UIButton!
+    
+    @IBOutlet weak var photoFilteringButton: UIButton!
+    
+    @IBOutlet weak var storeView: UIView!
+    
+    let imagePickerController = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello home")
+        photoResizingButton.isSelected = false
+        photoFilteringButton.isSelected = false
+        storeView.backgroundColor = .clear
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    func setImagePickerProperties() {
+        
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        imagePickerController.allowsEditing = true
+        imagePickerController.mediaTypes = [kUTTypeImage as String]
+    }
+    @IBAction func photoResizingButtonAction(_ sender: UIButton) {
+        photoResizingButton.isSelected = true
+        photoFilteringButton.isSelected = false
+        self.setImagePickerProperties()
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func photoFilteringButtonAction(_ sender: Any) {
+        photoResizingButton.isSelected = false
+        photoFilteringButton.isSelected = true
+        self.setImagePickerProperties()
+        self.present(imagePickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            picker.dismiss(animated: true, completion: nil)
+            return
+        }
+        //DispatchQueue.global(qos: .userInitiated).async {
+          //  DispatchQueue.main.async {
+                if(self.photoResizingButton.isSelected) {
+                    self.setResizeViewAsSubView(selectedImage: selectedImage)
+                } else if(self.photoFilteringButton.isSelected) {
+                    self.setFilterViewAsSubView(selectedImage: selectedImage)
+                }
+          //  }
+        //}
+        self.dismiss(animated: true, completion: nil)
+        
+        
+    }
+    
+    private func setResizeViewAsSubView(selectedImage : UIImage) {
+        self.removeAllSubViews()
+        let resizeView = ResizeView(frame: self.storeView.bounds)
+        resizeView.image = selectedImage
+        resizeView.photoSize = selectedImage.size
+        resizeView.delegate = self
+        resizeView.processing()
+        resizeView.layoutIfNeeded()
+        //resizeView.setupView()
+        self.storeView.addSubview(resizeView)
+    }
+    
+    private func setFilterViewAsSubView(selectedImage : UIImage) {
+        self.removeAllSubViews()
+    }
+    
+    private func removeAllSubViews() {
+        for view in self.storeView.subviews {
+            view.removeFromSuperview()
+        }
+    }
+    
+    func getPhotoToSave(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(savePhotoToLibrary(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func savePhotoToLibrary(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let alertController = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alertController, animated: true)
+        } else {
+            let alertController = UIAlertController(title: "Saved!", message: "Your image has been saved to your photos.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alertController, animated: true)
+        }
+    }
     /*
     // MARK: - Navigation
 
---
+++ Photo-Resizing-Filtering/Modules/HomePage/Views/ResizeView/ResizeView.swift
@@ -0,0 +1,163 @@
+//
+//  ResizeView.swift
+//  Photo-Resizing-Filtering
+//
+//  Created by sz ashik on 20/7/21.
+//
+
+import UIKit
+
+protocol SavePhotoToLibraryDelegate: AnyObject {
+    func getPhotoToSave(image : UIImage)
+}
+
+class ResizeView: UIView, UITextFieldDelegate {
+    
+    @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var originalPhotoSizeLabel: UILabel!
+    @IBOutlet weak var customWidthTextField: UITextField!
+    
+    @IBOutlet weak var customHeightTextField: UITextField!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    var image : UIImage?
+    var photoSize : CGSize?
+    weak var delegate : SavePhotoToLibraryDelegate?
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        //self.frame = frame
+        self.nibsetup(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func nibsetup(frame : CGRect) {
+        
+        let nib = UINib(nibName: "ResizeView", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame = frame
+        print("content view check frame = \(contentView.frame)")
+        addSubview(contentView)
+    }
+    
+    func processing() {
+        setImageToImageView()
+        setOriginalPhotoSizeDescription()
+        setTextFields()
+        
+    }
+    
+    func setOriginalPhotoSizeDescription() {
+        let photoWidth : Int = Int(photoSize!.width)
+        let photoHeight : Int = Int(photoSize!.height)
+        self.originalPhotoSizeLabel.text = "Original Size \(photoWidth) x \(photoHeight) pixel"
+    }
+    
+    func setImageToImageView() {
+        self.imageView.image = image
+    }
+    
+    func setTextFields() {
+        customWidthTextField.placeholder = "Width pixel"
+        customWidthTextField.font = UIFont.systemFont(ofSize: 15)
+        customWidthTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        customWidthTextField.autocorrectionType = UITextAutocorrectionType.no
+        customWidthTextField.keyboardType = UIKeyboardType.default
+        customWidthTextField.returnKeyType = UIReturnKeyType.done
+        customWidthTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+        customWidthTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        customWidthTextField.keyboardType = .numberPad
+        customWidthTextField.delegate = self
+        
+        customHeightTextField.placeholder = "Width pixel"
+        customHeightTextField.font = UIFont.systemFont(ofSize: 15)
+        customHeightTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        customHeightTextField.autocorrectionType = UITextAutocorrectionType.no
+        customHeightTextField.keyboardType = UIKeyboardType.default
+        customHeightTextField.returnKeyType = UIReturnKeyType.done
+        customHeightTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+        customHeightTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        customHeightTextField.keyboardType = .numberPad
+        customHeightTextField.delegate = self
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.endEditing(true)
+    }
+    @IBAction func saveButtonAction(_ sender: UIButton) {
+        guard let widthValue = Int(customWidthTextField.text!), let heightValue = Int(customHeightTextField.text!) else {
+            fatalError("SomeThing is wrong")
+           // return
+        }
+         let resizedImage : UIImage? = self.resizeImage(image: self.image!, newWidth: widthValue, newHeight: heightValue)!
+        if resizedImage != nil {
+            delegate?.getPhotoToSave(image: resizedImage!)
+        }
+    }
+    
+    func resizeImage(image: UIImage, newWidth: Int, newHeight : Int) -> UIImage? {
+
+        UIGraphicsBeginImageContext(CGSize(width: newWidth, height: newHeight))
+        image.draw(in: CGRect(x: 0, y: 0, width: newWidth, height: newHeight))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+    
+//    func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+//            if let error = error {
+//                // we got back an error!
+//                let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+//                ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                present(ac, animated: true)
+//            } else {
+//                let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+//                ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                present(ac, animated: true)
+//            }
+//        }
+    
+    func textFieldBeganEndEditing(_ textField: UITextField) {
+        print("text field began editing called")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("text field end editing called")
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if(textField == self.customWidthTextField) {
+            self.customHeightTextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        //textField.resignFirstResponder()
+        return true
+    }
+    
+    
+}
+
